5455d40d682ec83cf86a0ce11e130e205f5bf38b
BAROKA <birotori@gmail.com>
2020-09-09 16:02:14 +0100

    first commit

---
+++ db/migrate/20200909145834_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :name, precence:true, null:false ,uniqueness: true, length:{ maximum:60 }
+      t.text :content,precence:true,null:false
+
+      t.timestamps
+    end
+  end
+end
