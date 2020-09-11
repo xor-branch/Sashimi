6a351eeee2688e8463f6c40c1f85a43afaa3aebe
BAROKA <birotori@gmail.com>
2020-09-11 12:30:44 +0100

    Step 2 Basic various settings + Deployment requirements checklist

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  default_scope { order(created_at: :desc) }
   validates :name, presence:true, null:false,length:{maximum:60}
   validates :content, presence:true, null:false
 end
