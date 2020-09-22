fc08a2c5da07fcfccbcd0f6945c3a9f8e800947c
BAROKA <birotori@gmail.com>
2020-09-22 10:50:44 +0100

    add and delete roles on the management screen.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   end
   private
   def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name,:email,:admin,:password,:password_confirmation)
   end
   def set_user
     @user=User.find(params[:id])
